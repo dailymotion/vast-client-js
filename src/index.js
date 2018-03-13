b9be3ba3294733abec8e5a281c758586b381ab37
Elia Maino <eliamaino@gmail.com>
2018-03-13 17:49:25 +0100

    [ES6] convert index.coffee

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-module.exports =
-    client: require './client.coffee'
-    tracker: require './tracker.js'
-    parser: require './parser/parser.coffee'
-    util: require './util.js'
+module.exports = {
+    client: require('./client.coffee'),
+    tracker: require('./tracker.js'),
+    parser: require('./parser/parser.coffee'),
+    util: require('./util.js')
+};
