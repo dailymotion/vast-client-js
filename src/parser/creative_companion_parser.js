459596707774f9ddc83c7bd64c6d6a6f4b1fb21e
Elia Maino <eliamaino@gmail.com>
2018-03-13 17:22:30 +0100

    Refactoring fixes after CreativeCompanionParser es6 conversion

--- src/parser/creative_companion_parser.js
+++ src/parser/creative_companion_parser.js
@@ -1,9 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const VASTCompanionAd = require('../companionad.js');
 const { VASTCreativeCompanion } = require('../creative.js');
 const ParserUtils = require('./parser_utils.js');
@@ -16,29 +10,29 @@ class CreativeCompanionParser {
     parse(creativeElement, creativeAttributes) {
         const creative = new VASTCreativeCompanion(creativeAttributes);
 
-        for (let companionResource of Array.from(this.utils.childrenByName(creativeElement, "Companion"))) {
+        for (let companionResource of this.utils.childrenByName(creativeElement, "Companion")) {
             const companionAd = new VASTCompanionAd();
             companionAd.id = companionResource.getAttribute("id") || null;
             companionAd.width = companionResource.getAttribute("width");
             companionAd.height = companionResource.getAttribute("height");
             companionAd.companionClickTrackingURLTemplates = [];
-            for (let htmlElement of Array.from(this.utils.childrenByName(companionResource, "HTMLResource"))) {
+            for (let htmlElement of this.utils.childrenByName(companionResource, "HTMLResource")) {
                 companionAd.type = htmlElement.getAttribute("creativeType") || 'text/html';
                 companionAd.htmlResource = this.utils.parseNodeText(htmlElement);
             }
-            for (let iframeElement of Array.from(this.utils.childrenByName(companionResource, "IFrameResource"))) {
+            for (let iframeElement of this.utils.childrenByName(companionResource, "IFrameResource")) {
                 companionAd.type = iframeElement.getAttribute("creativeType") || 0;
                 companionAd.iframeResource = this.utils.parseNodeText(iframeElement);
             }
-            for (let staticElement of Array.from(this.utils.childrenByName(companionResource, "StaticResource"))) {
+            for (let staticElement of this.utils.childrenByName(companionResource, "StaticResource")) {
                 companionAd.type = staticElement.getAttribute("creativeType") || 0;
-                for (let child of Array.from(this.utils.childrenByName(companionResource, "AltText"))) {
+                for (let child of this.utils.childrenByName(companionResource, "AltText")) {
                     companionAd.altText = this.utils.parseNodeText(child);
                 }
                 companionAd.staticResource = this.utils.parseNodeText(staticElement);
             }
-            for (let trackingEventsElement of Array.from(this.utils.childrenByName(companionResource, "TrackingEvents"))) {
-                for (let trackingElement of Array.from(this.utils.childrenByName(trackingEventsElement, "Tracking"))) {
+            for (let trackingEventsElement of this.utils.childrenByName(companionResource, "TrackingEvents")) {
+                for (let trackingElement of this.utils.childrenByName(trackingEventsElement, "Tracking")) {
                     const eventName = trackingElement.getAttribute("event");
                     const trackingURLTemplate = this.utils.parseNodeText(trackingElement);
                     if ((eventName != null) && (trackingURLTemplate != null)) {
@@ -47,7 +41,7 @@ class CreativeCompanionParser {
                     }
                 }
             }
-            for (let clickTrackingElement of Array.from(this.utils.childrenByName(companionResource, "CompanionClickTracking"))) {
+            for (let clickTrackingElement of this.utils.childrenByName(companionResource, "CompanionClickTracking")) {
                 companionAd.companionClickTrackingURLTemplates.push(this.utils.parseNodeText(clickTrackingElement));
             }
             companionAd.companionClickThroughURLTemplate = this.utils.parseNodeText(this.utils.childByName(companionResource, "CompanionClickThrough"));
