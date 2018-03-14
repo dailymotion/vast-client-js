a164d5a43036d75f41637a07d0494e855a749b87
Elia Maino <eliamaino@gmail.com>
2018-03-14 13:52:17 +0100

    Refactoring fixes after test/urlhandler.coffee es6 conversion

--- test/urlhandler.js
+++ test/urlhandler.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const should = require('should');
 const path = require('path');
 const URLHandler = require('../src/urlhandler');
@@ -15,29 +10,29 @@ describe('URLHandler', () =>
     describe('#get', function() {
         it('should return options.response when it\'s provided', done => {
             const options = {response: 'response'};
-            return urlHandler.get(urlfor('sample.xml'), options, function(err, xml) {
+            urlHandler.get(urlfor('sample.xml'), options, function(err, xml) {
                 should.not.exist(err);
                 should.exists(xml);
                 xml.should.equal('response');
                 should.not.exist(options.response);
-                return done();
+                done();
             });
         });
 
         it('should return a VAST XML DOM object', done => {
-            return urlHandler.get(urlfor('sample.xml'), function(err, xml) {
+            urlHandler.get(urlfor('sample.xml'), function(err, xml) {
                 should.not.exist(err);
                 should.exists(xml);
                 xml.documentElement.nodeName.should.equal('VAST');
-                return done();
+                done();
             });
         });
 
-        return it('should return an error if not found', done => {
-            return urlHandler.get(urlfor('not-found.xml'), function(err, xml) {
+        it('should return an error if not found', done => {
+            urlHandler.get(urlfor('not-found.xml'), function(err, xml) {
                 should.exists(err);
                 should.not.exist(xml);
-                return done();
+                done();
             });
         });
     })
