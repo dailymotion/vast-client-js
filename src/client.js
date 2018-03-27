ec20b31f32bd569191cf28674f76de8fbd6f4605
Elia Maino <eliamaino@gmail.com>
2018-03-27 14:07:14 +0200

    Improve documentation comments and Storage

--- src/client.js
+++ src/client.js
@@ -2,7 +2,20 @@ import { Storage } from './util/storage';
 import { Util } from './util/util';
 import { VASTParser } from './parser/parser';
 
+/**
+ * This class provides methods to fetch and parse a VAST document using VASTParser.
+ * In addition it provides options to skip consecutive calls based on constraints.
+ * @export
+ * @class VASTClient
+ */
 export class VASTClient {
+  /**
+   * Creates an instance of VASTClient.
+   * @param  {Number} cappingFreeLunch - The number of first calls to skip.
+   * @param  {Number} cappingMinimumTimeInterval - The minimum time interval between two consecutive calls.
+   * @param  {Storage} customStorage - A custom storage to use instead of the default one.
+   * @constructor
+   */
   constructor(cappingFreeLunch, cappingMinimumTimeInterval, customStorage) {
     this.cappingFreeLunch = cappingFreeLunch || 0;
     this.cappingMinimumTimeInterval = cappingMinimumTimeInterval || 0;
@@ -27,29 +40,36 @@ export class VASTClient {
   }
 
   get lastSuccessfullAd() {
-    return this.storage.getItem('lastSuccessfullAd');
+    return this.storage.getItem('vast-client-last-successfull-ad');
   }
 
   set lastSuccessfullAd(value) {
-    this.storage.setItem('lastSuccessfullAd', value);
+    this.storage.setItem('vast-client-last-successfull-ad', value);
   }
 
   get totalCalls() {
-    return this.storage.getItem('totalCalls');
+    return this.storage.getItem('vast-client-total-calls');
   }
 
   set totalCalls(value) {
-    this.storage.setItem('totalCalls', value);
+    this.storage.setItem('vast-client-total-calls', value);
   }
 
   get totalCallsTimeout() {
-    return this.storage.getItem('totalCallsTimeout');
+    return this.storage.getItem('vast-client-total-calls-timeout');
   }
 
   set totalCallsTimeout(value) {
-    this.storage.setItem('totalCallsTimeout', value);
+    this.storage.setItem('vast-client-total-calls-timeout', value);
   }
 
+  /**
+   * Gets a parsed VAST document for the given url, applying the skipping rules defined.
+   * Executes the callback with either an Error or the fully parsed VASTResponse.
+   * @param    {String} url - The url to use to fecth the VAST document.
+   * @param    {Object} options - An optional Object of parameters to be applied in the process.
+   * @callback cb
+   */
   get(url, options, cb) {
     // options parameter is optional
     if (!cb) {
--- src/util/storage.js
+++ src/util/storage.js
@@ -1,10 +1,24 @@
 let storage = null;
 
+/**
+ * This class provides an wrapper interface to the a key-value storage.
+ * It uses localStorage, sessionStorage or a custom storage if none of the two is available.
+ * @export
+ * @class Storage
+ */
 export class Storage {
+  /**
+   * Creates an instance of Storage.
+   * @constructor
+   */
   constructor() {
     this.storage = this.initStorage();
   }
 
+  /**
+   * Provides a singleton instance of the wrapped storage.
+   * @return {Object}
+   */
   initStorage() {
     if (storage != null) {
       return storage;
@@ -24,14 +38,16 @@ export class Storage {
     // http://spin.atomicobject.com/2013/01/23/ios-private-browsing-localstorage/
     const isDisabled = function(store) {
       try {
-        const testValue = '__VASTUtil__';
+        const testValue = '__VASTStorage__';
         store.setItem(testValue, testValue);
         if (store.getItem(testValue) !== testValue) {
+          store.removeItem(testValue);
           return true;
         }
       } catch (e) {
         return true;
       }
+      store.removeItem(testValue);
       return false;
     };
 
@@ -60,18 +76,37 @@ export class Storage {
     return storage;
   }
 
+  /**
+   * Returns the value for the given key. If the key does not exist, null is returned.
+   * @param  {String} key - The key to retrieve the value.
+   * @return {any}
+   */
   getItem(key) {
     return this.storage.getItem(key);
   }
 
+  /**
+   * Adds or updates the value for the given key.
+   * @param  {String} key - The key to modify the value.
+   * @param  {any} value - The value to be associated with the key.
+   * @return {any}
+   */
   setItem(key, value) {
     return this.storage.setItem(key, value);
   }
 
+  /**
+   * Removes an item for the given key.
+   * @param  {String} key - The key to remove the value.
+   * @return {any}
+   */
   removeItem(key) {
     return this.storage.removeItem(key);
   }
 
+  /**
+   * Removes all the items from the storage.
+   */
   clear() {
     return this.storage.clear();
   }
