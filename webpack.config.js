564957b9a32d2568e5dfc51a1668e509bb3a2ce6
Elia Maino <eliamaino@gmail.com>
2018-07-24 15:44:48 +0200

    [tools] Add separate entrypoint for node

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-const path = require('path');
-
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: __dirname,
-    filename: 'vast-client.js',
-    library: 'VAST'
-  },
-  node: {
-    fs: 'empty'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env'],
-            plugins: ['transform-object-assign']
+module.exports = env => {
+  const config = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      path: __dirname
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel-preset-env'],
+              plugins: ['transform-object-assign']
+            }
           }
         }
-      }
-    ]
+      ]
+    }
+  };
+
+  if (env === 'node') {
+    config.target = 'node';
+    config.output.filename = 'vast-client-node.js';
+    config.output.libraryTarget = 'umd';
+  } else {
+    config.output.filename = 'vast-client.js';
+    config.output.library = 'VAST';
+    config.node = {
+      fs: 'empty'
+    };
   }
+
+  return config;
 };
