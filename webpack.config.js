23ca9f60dfd52c3f0e0f51b2d2e13e611aac4578
Elia Maino <eliamaino@gmail.com>
2018-07-25 11:33:33 +0200

    [tools] Don't include node urlhandler in browser targeted build

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = env => {
         {
           test: /\.js$/,
           exclude: /(node_modules)/,
+          use: ['webpack-conditional-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
@@ -19,14 +24,23 @@ module.exports = env => {
           }
         }
       ]
+    },
+    resolve: {
+      alias: {}
     }
   };
 
   if (env === 'node') {
+    config.resolve.alias = {
+      nodeUrlHandler: './urlhandlers/node_url_handler.js'
+    };
     config.target = 'node';
     config.output.filename = 'vast-client-node.js';
     config.output.libraryTarget = 'umd';
   } else {
+    config.resolve.alias = {
+      nodeUrlHandler: './urlhandlers/mock_node_url_handler.js'
+    };
     config.output.filename = 'vast-client.js';
     config.output.library = 'VAST';
     config.node = {
