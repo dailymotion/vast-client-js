14a7e8f5a86e5b76751afadc0e5275281fd36f79
Elia Maino <eliamaino@gmail.com>
2018-07-25 14:52:17 +0200

    [tools] Add some comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -32,16 +32,19 @@ module.exports = env => {
     }
   };
 
+  // Bundle non-minified version of the library
   if (env === 'dev' || env === 'node-dev') {
     config.mode = 'development';
   }
 
+  // Options for node bundling
   if (env === 'node' || env === 'node-dev') {
     config.target = 'node';
     config.output.filename =
       env === 'node-dev' ? 'vast-client-node.js' : 'vast-client-node.min.js';
     config.output.libraryTarget = 'umd';
   } else {
+    // Options for browser bundling
     config.resolve.alias['./urlhandlers/node_url_handler'] =
       './urlhandlers/mock_node_url_handler';
     config.output.filename =
