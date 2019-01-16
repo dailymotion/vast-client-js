741dc7b8dbc94d37eb7b46358d9d5d05c069c89f
Clément FRANCON <c.francon@daily.local>
2019-01-16 17:32:07 +0100

    [parser][test] refactor of how extensions are parsed to handle nested xml

--- src/ad_extension_child.js
+++ src/ad_extension_child.js
@@ -3,5 +3,15 @@ export class AdExtensionChild {
     this.name = null;
     this.value = null;
     this.attributes = {};
+    this.children = [];
+  }
+
+  isEmpty() {
+    return (
+      this.name === null &&
+      this.value === null &&
+      Object.keys(this.attributes).length === 0 &&
+      this.children.length === 0
+    );
   }
 }
