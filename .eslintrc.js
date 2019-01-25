527da8b5617816a149653466c2e0107e0a62da57
Elia Maino <eliamaino@gmail.com>
2019-01-25 15:10:45 +0100

    [tools] Add array return rule for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: ['import'],
   rules: {
+    'array-callback-return': ['error'],
     camelcase: ['warn'],
     eqeqeq: ['error'],
     'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
