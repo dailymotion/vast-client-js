67af165f5fe16c61ccadef9b85a82aaf94390ed4
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-02-13 15:56:52 +0100

    [test] Convert parser utils tests to jest

--- test/vast_parser.js
+++ test/vast_parser.js
@@ -140,46 +140,6 @@ describe('VASTParser', function() {
       this.response.should.be.an.instanceOf(VASTResponse);
     });
 
-    describe('#duration', () =>
-      [
-        null,
-        undefined,
-        -1,
-        0,
-        1,
-        '1',
-        '00:00',
-        '00:00:00:00',
-        'test',
-        '00:test:01',
-        '00:00:01.001',
-        '00:00:01.test'
-      ].map(item =>
-        (item =>
-          it(`should not return NaN for \`${item}\``, () =>
-            isNaN(parserUtils.parseDuration(item)).should.eql(false)))(item)
-      ));
-
-    describe('#duration', () =>
-      [
-        null,
-        undefined,
-        -1,
-        0,
-        1,
-        '1',
-        '00:00',
-        '00:00:00:00',
-        'test',
-        '00:test:01',
-        '00:00:01.001',
-        '00:00:01.test'
-      ].map(item =>
-        (item =>
-          it(`should not return NaN for \`${item}\``, () =>
-            isNaN(parserUtils.parseDuration(item)).should.eql(false)))(item)
-      ));
-
     describe('#For the 1st ad', function() {
       let ad1 = null;
 
