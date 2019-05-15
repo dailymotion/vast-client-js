e00203a4c823a59f8fb61cf814e5ca1b413d79c3
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-05-15 12:59:35 +0200

    [util] Add extra type safety to joinArrayUnique

--- src/util/util.js
+++ src/util/util.js
@@ -115,7 +115,10 @@ function flatten(arr) {
  * @param {Array} arr2
  */
 function joinArrayUnique(arr1 = [], arr2 = []) {
-  const arr = (arr1 || []).concat(arr2 || []);
+  const firstArr = Array.isArray(arr1) ? arr1 : [];
+  const secondArr = Array.isArray(arr2) ? arr2 : [];
+  const arr = firstArr.concat(secondArr);
+
   return arr.reduce((res, val) => {
     if (res.indexOf(val) === -1) {
       res.push(val);
