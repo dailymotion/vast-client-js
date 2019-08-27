6a81d0386e3e0e7cc9ce695ef721913bd072be83
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2019-08-27 15:46:20 +0200

    [parser][util][test] VAST 4.1 Mezzanine element support

---
+++ src/mezzanine.js
@@ -0,0 +1,13 @@
+export class Mezzanine {
+  constructor() {
+    this.id = null;
+    this.fileURL = null;
+    this.delivery = null;
+    this.codec = null;
+    this.type = null;
+    this.width = 0;
+    this.height = 0;
+    this.fileSize = 0;
+    this.mediaType = '2D';
+  }
+}
