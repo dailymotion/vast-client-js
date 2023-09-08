83fae5b42532fc2e2a9f71f79b948ab61f102ac4
Raphael Levin <r.levin@dailymotion.com>
2023-09-08 12:23:03 +0200

    wip/upgrating urlhandler

--- src/urlhandlers/xhr_url_handler.js
+++ src/urlhandlers/xhr_url_handler.js
@@ -1,67 +1,122 @@
 import { DEFAULT_TIMEOUT } from './consts';
 
-function xhr() {
-  try {
-    const request = new window.XMLHttpRequest();
-    if ('withCredentials' in request) {
-      // check CORS support
-      return request;
+// function supported() {
+//   return !!xhr();
+// }
+
+// // si succes
+// function handleLoad(request, cb) {
+//   if (request.status === 200) {
+//     cb(null, request.responseXML, {
+//       byteLength: request.response.length,
+//       statusCode: request.status,
+//     });
+//   } else {
+//     handleFail(request, cb, false);
+//   }
+// }
+
+// // si foireux
+// function handleFail(request, cb, isTimeout) {
+//   const statusCode = !isTimeout ? request.status : 408; // Request timeout
+//   const msg = isTimeout
+//     ? `XHRURLHandler: Request timed out after ${request.timeout} ms (${statusCode})`
+//     : `XHRURLHandler: ${request.statusText} (${statusCode})`;
+
+//   cb(new Error(msg), null, { statusCode });
+// }
+
+// function get(url, options, cb) {
+//   if (window.location.protocol === 'https:' && url.indexOf('http://') === 0) {
+//     return cb(new Error('XHRURLHandler: Cannot go from HTTPS to HTTP.'));
+//   }
+
+//   try {
+//     const request = xhr();
+
+//     request.open('GET', url);
+//     request.timeout = options.timeout || DEFAULT_TIMEOUT;
+//     request.withCredentials = options.withCredentials || false;
+//     request.overrideMimeType && request.overrideMimeType('text/xml');
+//     request.onload = () => handleLoad(request, cb);
+//     request.onerror = () => handleFail(request, cb, false);
+//     request.onabort = () => handleFail(request, cb, false);
+//     request.ontimeout = () => handleFail(request, cb, true);
+
+//     request.send();
+//   } catch (error) {
+//     cb(new Error('XHRURLHandler: Unexpected error'));
+//   }
+// }
+
+function isBrowserEnvironment() {
+  return typeof window !== 'undefined';
+}
+
+/**
+ * handlingResponse
+ * @param {*} request
+ * @returns
+ */
+async function handleResponse(request) {
+  let details = {};
+  const textXml = await request.text();
+
+  details.byteLength = textXml.length;
+  details.statusCode = request.status;
+
+  const parser = isBrowserEnvironment() ? new DOMParser() : new nodeDOMParser();
+  const xml = parser.parseFromString(textXml, 'text/xml');
+  return { xml, details };
+}
+/**
+ * Error Handling
+ * @param {*} request
+ * @returns
+ */
+function handleError(request) {
+  if (isBrowserEnvironment()) {
+    if (
+      window.location.protocol === 'http:' &&
+      request.url.indexOf('https://') === 0
+    ) {
+      return 'XHRURLHandler: Cannot go from HTTPS to HTTP.';
     }
-    return null;
-  } catch (err) {
-    return null;
   }
-}
-
-function supported() {
-  return !!xhr();
-}
-
-function handleLoad(request, cb) {
-  if (request.status === 200) {
-    cb(null, request.responseXML, {
-      byteLength: request.response.length,
-      statusCode: request.status,
-    });
-  } else {
-    handleFail(request, cb, false);
+  if (request.status !== 200 || !request.ok) {
+    return `XHRURLHandler: ${request.statusText} (${request.status})`;
   }
+  return null;
 }
 
-function handleFail(request, cb, isTimeout) {
-  const statusCode = !isTimeout ? request.status : 408; // Request timeout
-  const msg = isTimeout
-    ? `XHRURLHandler: Request timed out after ${request.timeout} ms (${statusCode})`
-    : `XHRURLHandler: ${request.statusText} (${statusCode})`;
-
-  cb(new Error(msg), null, { statusCode });
-}
-
-function get(url, options, cb) {
-  if (window.location.protocol === 'https:' && url.indexOf('http://') === 0) {
-    return cb(new Error('XHRURLHandler: Cannot go from HTTPS to HTTP.'));
-  }
-
+async function get(url, options) {
   try {
-    const request = xhr();
+    ///// timeout
+    const controller = new AbortController();
+    const timer = setTimeout(() => {
+      controller.abort();
+      throw new Error(
+        `XHRURLHandler: Request timed out after ${options.timeout} ms (408)`
+      );
+    }, options.timeout);
+    /////
 
-    request.open('GET', url);
-    request.timeout = options.timeout || DEFAULT_TIMEOUT;
-    request.withCredentials = options.withCredentials || false;
-    request.overrideMimeType && request.overrideMimeType('text/xml');
+    const request = await fetch(url, { ...options, signal: controller.signal });
+    clearTimeout(timer);
 
-    request.onload = () => handleLoad(request, cb);
-    request.onerror = () => handleFail(request, cb, false);
-    request.onabort = () => handleFail(request, cb, false);
-    request.ontimeout = () => handleFail(request, cb, true);
-
-    request.send();
+    const error = handleError(request);
+    if (error) {
+      return { error: new Error(error) };
+    }
+    return handleResponse(request);
   } catch (error) {
-    cb(new Error('XHRURLHandler: Unexpected error'));
+    return {
+      error,
+      statusCode: error.name === 'AbortError' ? 408 : undefined,
+    };
   }
 }
 
-export const XHRURLHandler = {
+export const urlHandler = {
   get,
-  supported,
 };
