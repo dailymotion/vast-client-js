b41edc3f4942a9e3abefa85c68ac1a3a6816d222
Raphael Levin <r.levin@dailymotion.com>
2023-10-04 14:45:42 +0200

    [test] correcting typo error

--- spec/samples/inline_trackers.js
+++ spec/samples/inline_trackers.js
@@ -234,7 +234,7 @@ export const inlineTrackersParsed = {
                 'https://www.iab.com/wp-content/uploads/2014/09/iab-tech-lab-6-644x290.png',
               htmlResource:
                 '<!DOCTYPE html>\n                                            <html lang="en">\n                                            <head>\n                                                <meta charset="UTF-8">\n                                                <meta http-equiv="X-UA-Compatible" content="IE=edge">\n                                                <meta name="viewport" content="width=device-width, initial-scale=1.0">\n                                                <title>Document</title>\n                                            </head>\n                                            <body>\n                                                <h1>titre</h1>\n                                            </body>\n                                            </html>',
-              iframeResource: 'lient vers html',
+              iframeResource: 'liens vers html',
               nonlinearClickThroughURLTemplate: 'https://iabtechlab.com',
               nonlinearClickTrackingURLTemplates: [
                 {
