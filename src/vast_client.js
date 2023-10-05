be09e552507549cf097499734831e0c3cb558959
Raphael Levin <r.levin@dailymotion.com>
2023-10-05 12:26:18 +0200

    [client] refactor constructor default values

--- src/vast_client.js
+++ src/vast_client.js
@@ -16,12 +16,16 @@ export class VASTClient {
    * @param  {Storage} customStorage - A custom storage to use instead of the default one.
    * @constructor
    */
-  constructor(cappingFreeLunch, cappingMinimumTimeInterval, customStorage) {
-    this.cappingFreeLunch = cappingFreeLunch || 0;
-    this.cappingMinimumTimeInterval = cappingMinimumTimeInterval || 0;
+  constructor(
+    cappingFreeLunch = 0,
+    cappingMinimumTimeInterval = 0,
+    customStorage = new Storage()
+  ) {
+    this.cappingFreeLunch = cappingFreeLunch;
+    this.cappingMinimumTimeInterval = cappingMinimumTimeInterval;
     this.fetcher = new Fetcher();
     this.vastParser = new VASTParser({ fetcher: this.fetcher });
-    this.storage = customStorage || new Storage();
+    this.storage = customStorage;
     // Init values if not already set
     if (this.lastSuccessfulAd === undefined) {
       this.lastSuccessfulAd = 0;
